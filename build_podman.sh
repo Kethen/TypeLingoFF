9c13cca7435808c9bfc09ce72e631e8b1f444a9a
Katharine Chui <kwchuiaa@connect.ust.hk>
2025-08-11 21:37:23 +0200

    add build CI

---
+++ build_podman.sh
@@ -0,0 +1,22 @@
+set -xe
+
+IMAGE_TAG="typelingoff"
+
+if [ "$REBUILD_IMAGE" == "true" ]
+then
+	podman image remove -f $IMAGE_TAG
+fi
+
+if ! podman image exists $IMAGE_TAG
+then
+	podman image build -f Dockerfile -t $IMAGE_TAG
+fi
+
+podman run --rm -it \
+	-v ./:/workdir \
+	-w /workdir \
+	--entrypoint /bin/bash \
+	$IMAGE_TAG \
+	-c '
+	web-ext build
+'
